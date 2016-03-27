2ecd27d8b9e28ae08ecebaa8e6ab44b14f75f19b
codecyl <codeest@126.com>
2016-03-27 22:40:29 +0800

    Create 1.swift

---
+++ Swift/1.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func getNaturalLog()->Double
+{
+    var index:Double = 1
+    var item:Double = 1
+    var result:Double = 0
+    
+    while (item >= 1e-4) {
+        result += item
+        item /= index++
+    }
+    
+    return result
+}
+
+print(String(getNaturalLog()))
