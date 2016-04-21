54d3e164cfe7d1c942cfb4b30dee27fe6ba7f1da
jasonHzq <hzongquan@126.com>
2016-04-21 21:41:23 +0800

    fix(2.js): fix 2.js

--- ECMAScript/2.js
+++ ECMAScript/2.js
@@ -1,7 +1,7 @@
 function sumPrimes(num) {
-  const primes = [2, 3, 5, 7];
-  
-  for (let i = 8; i <= num; ++i) {
+  const primes = [];
+
+  for (let i = 2; i <= num; ++i) {
     let isPrime = true;
 
     for (let j = 0, length = primes.length; j < length; ++j) {
